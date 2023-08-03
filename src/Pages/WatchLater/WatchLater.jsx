b2a5682b1ee6bb1ee8263f470f31145a7e9d4eed
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-03 23:55:32 +0530

    add tio watch later added

--- src/Pages/WatchLater/WatchLater.jsx
+++ src/Pages/WatchLater/WatchLater.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import {AiFillEye} from "react-icons/ai";
 
 import NavBar from "../../Components/NavBar/NavBar";
+import { DataContext } from '../../Context/DataContext';
 import "./WatchLater.css";
 
 const WatchLater = () => {
+  const { watch_later } = useContext(DataContext);
+  console.log(watch_later, "watchLater Page")
   return (
     <div className='watch-later'>
       <div className='navbar-side'>
@@ -13,6 +17,28 @@ const WatchLater = () => {
         <h3 className='watchlater-text'>
           Watch Later
         </h3>
+        <div watchlater-video>
+          {
+            watch_later.length > 0 ? watch_later.map((video) =>
+              <div className='watch-later-container' key={video._id}>
+                <div className='video-container'>
+                  <div className='video-thumbnail'>
+                    <img src={video.thumbnail} alt='origami' />
+                  </div>
+                  <p className='video-title'>{video.title}</p>
+                  <div className='video-views-creator'>
+                    <p className='views'><AiFillEye />{video.views}</p>
+                    <p className='creator'>{video.creator}</p>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <p className='add-to-watch-later'>
+                Add Videos to watch later
+              </p>
+            )
+          }
+        </div>
       </div>
     </div>
   )
