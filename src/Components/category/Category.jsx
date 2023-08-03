1c74ab5cc7b259465d67030f9646a8042fe23c01
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-03 23:54:03 +0530

    video component added

--- src/Components/category/Category.jsx
+++ src/Components/category/Category.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useParams, Link } from "react-router-dom";
-import { AiFillEye } from "react-icons/ai"
+import { useParams} from "react-router-dom";
+
 
 import NavBar from '../NavBar/NavBar';
 import { videos } from "../../Data/VideosData";
 import "./Category.css";
+import Video from '../Video/Video';
 
 const Category = () => {
     const { category } = useParams();
@@ -24,20 +25,7 @@ const Category = () => {
                 <div className='video-box-container'>
                     {
                         filterVideo && filterVideo.map((video) => (
-                            <div className='video-box' key={video._id}>
-                                <Link to={`/category/${video._id}`} className='video-links'>
-                                    <div className='video-container'>
-                                        <div className='video-thumbnail'>
-                                            <img src={video.thumbnail} alt='origami' />
-                                        </div>
-                                        <p className='video-title'>{video.title}</p>
-                                        <div className='video-views-creator'>
-                                            <p className='views'><AiFillEye />{video.views}</p>
-                                            <p className='creator'>{video.creator}</p>
-                                        </div>
-                                    </div>
-                                </Link>
-                            </div>
+                           <Video video = {video}/>
                         ))
                     }
                 </div>
