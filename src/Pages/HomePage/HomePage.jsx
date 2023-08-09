647ad381ff195af43199e6262fc5f4812e860a7d
BlackxJoker <arnavmourya7654@gmail.com>
2023-08-09 10:40:19 +0530

    code formatting

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,41 +1,39 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 import { categories } from "../../Data/CategoriesData";
 import "./HomePage.css";
-import NavBar from '../../Components/NavBar/NavBar';
+import NavBar from "../../Components/NavBar/NavBar";
 
 const HomePage = () => {
-    // console.log(categories);
-    return (
-        <div className='homepage'>
-            <div className='navbar-side'>
-                <NavBar />
-            </div>
-            <div className='categories'>
-                <h3 className='categories-list'>Categories</h3>
-                <div className='category-data'>
-                    {
-                        categories && categories.map((category) => (
-                            <div className='category-conatiner' key={category._id}>
-                                <Link to={`/${category.category}`}>
-                                    <div className='category-box'>
-                                        <div className='img-box'>
-                                            <img src={category.thumbnail} alt='thumbnail' />
-
-                                        </div>
-                                        <div className='category-name'>
-                                            <p className='category-name-text'>{category.category}</p>
-                                        </div>
-                                    </div>
-                                </Link>
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
-        </div >
-    )
-}
+  // console.log(categories);
+  return (
+    <div className="homepage">
+      <div className="navbar-side">
+        <NavBar />
+      </div>
+      <div className="categories">
+        <h3 className="categories-list">Categories</h3>
+        <div className="category-data">
+          {categories &&
+            categories.map((category) => (
+              <div className="category-conatiner" key={category._id}>
+                <Link to={`/${category.category}`}>
+                  <div className="category-box">
+                    <div className="img-box">
+                      <img src={category.thumbnail} alt="thumbnail" />
+                    </div>
+                    <div className="category-name">
+                      <p className="category-name-text">{category.category}</p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
