08923c2df75cecba6b62045bf9210a87ad0477cd
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-09 23:39:20 +0530

    change in payload

--- src/Components/AddNote/AddNote.jsx
+++ src/Components/AddNote/AddNote.jsx
@@ -1,37 +1,38 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
+import { v4 as uuid } from "uuid";
 
-import { DataContext } from '../../Context/DataContext';
+import { DataContext } from "../../Context/DataContext";
 import "./AddNote.css";
-import { useParams } from 'react-router';
+import { useParams } from "react-router";
 
 const AddNote = () => {
-    const {id} = useParams();
-    const [note, setNote] = useState({
-        _id: id,
-        text: "",
+  const { id } = useParams();
+  const [note, setNote] = useState("");
+  const { addDataDispatch} = useContext(DataContext);
+
+  // console.log(note, "note");
+
+  const addNotesHandler = () => {
+    addDataDispatch({
+      type: "add_notes",
+      payload: { text: note, videoId: id, _id: uuid() },
     });
-    const{addDataDispatch} = useContext(DataContext);
-
-    // console.log(note, "note");
-
-    const addNotesHandler = () => {
-        addDataDispatch({type: "add_notes", payload: note});
-    };
-    return (
-        <div className='add-note'>
-            <div className='add-note-conatiner'>
-                <input
-                    className='add-note-input'
-                    type='text'
-                    placeholder='Add Notes'
-                    onChange={(event) => setNote((prevState) => ({
-                        ...prevState, text: event.target.value,
-                    }))}
-                />
-                <button className='add-note-btn' onClick={addNotesHandler}>Add Note</button>
-            </div>
-        </div>
-    )
-}
+  };
+  return (
+    <div className="add-note">
+      <div className="add-note-conatiner">
+        <input
+          className="add-note-input"
+          type="text"
+          placeholder="Add Notes"
+          onChange={(event) => setNote(event.target.value)}
+        />
+        <button className="add-note-btn" onClick={addNotesHandler}>
+         Add Note
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default AddNote;
