057853c467c305315ab2dc69c434a470b893375a
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-10 00:37:12 +0530

    playlist

--- src/Reducer/DataReducer.js
+++ src/Reducer/DataReducer.js
@@ -1,18 +1,64 @@
+const getDataFromLocalStorage = (key) => {
+  let data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : [];
+};
+
+const noteListFromLocalStorage = getDataFromLocalStorage("notes");
+const watchLaterFromLocalStorage = getDataFromLocalStorage("watchLater");
+const playListFromLocalStorage = getDataFromLocalStorage("playlist");
+
+
 export const initial_state = {
-    notes: [],
-}
+  notes: noteListFromLocalStorage,
+  watch_later: [],
+  playlist: [
+  {
+    id: 1,
+    name: "music",
+    details: "dgfhhgfjj",
+    listItems: [ 
+        {
+          _id: 19,
+          title: 'Origami Swan - Simple and Elegant',
+          views: 2879,
+          chips: ['origami', 'swan', 'paper', 'elegant'],
+          thumbnail: 'https://picsum.photos/300/174',
+          src: 'https://www.youtube.com/embed/GBIIQ0kP15E',
+          category: 'Origami',
+          creator: 'PaperCraftPro',
+        },
+    ]
+  }
+  ],
+};
 
 export const reducer = (state, action) => {
-    const { type, payload } = action;
+  const { type, payload } = action;
 
-    switch (type) {
-        case "add_notes":
-            return {
-                ...state,
-                notes: [...state.notes, payload]
-            };
-        default:
-            return state;
-    }
-
+  switch (type) {
+    case "add_notes":
+      return {
+        ...state,
+        notes: [...state.notes, payload],
+      };
+    case "delete_note":
+      return {
+        ...state,
+        notes: [...state.notes].filter((note)=> note._id !== payload)
+      };
+    case "watch_video_later":
+      return {
+        ...state,
+        watch_later: [...state.watch_later, payload],
+      };
+    case "remove_from_watch_later":
+      return {
+        ...state,
+        watch_later: [...state.watch_later].filter(
+          (video) => video._id !== Number(payload)
+        ),
+      };
+    default:
+      return state;
+  }
 };
