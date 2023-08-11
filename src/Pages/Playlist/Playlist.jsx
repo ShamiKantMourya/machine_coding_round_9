e099bef54a3e0880d8947bd2a65ca4bfb6d83ee7
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-11 10:00:34 +0530

    code formatting

--- src/Pages/Playlist/Playlist.jsx
+++ src/Pages/Playlist/Playlist.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
+import { IoAddCircleOutline } from 'react-icons/io5';
 
 import NavBar from '../../Components/NavBar/NavBar';
+import { DataContext } from '../../Context/DataContext';
 import "./Playlist.css";
 
 const Playlist = () => {
+const {playlist} = useContext(DataContext);
+const [playlistModel, setPlaylistModel] = useState(false);
+console.log(playlist);
+
   return (
     <div className='playlist'>
     <div className='navbar-side'>
@@ -11,6 +17,23 @@ const Playlist = () => {
     </div>
     <div className='playlist-section'>
         <h3 className='playlist-list'>Playlists</h3>
+        <div className='playlist-options'>
+          {
+            playlist.length > 0 ? (
+              <div>
+
+              </div>
+            ) : (
+              <div className='create-playlist' onClick={() => setPlaylistModel(!playlistModel)}>
+                <div>
+                <p className='create-playlist-text'>Click to</p>
+                <p className='create-playlist-text'>Create a playlist</p>
+                </div>
+                <button><IoAddCircleOutline /></button>
+              </div>
+            )
+          }
+        </div>
     </div>
 </div>
   )
